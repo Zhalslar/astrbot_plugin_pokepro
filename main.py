0a4f142e3798feafa768b0c9852eddd10ed3be5d
翎羽_FDMNya <fdmnya@fdmnya.top>
2025-11-20 00:44:34 +0800

    fix: 修复无法获取人设 prompt 导致的无法调用LLM回复bug
    - 在高版本中，Provider 的 curr_personality 属性已被移除，无法再通过 using_provider.curr_personality 获取 personality，导致无法调用LLM回复
    - 通过 conversation.persona_id 和 persona_manager.get_default_persona_v3 来获取 persona_id，再通过 persona_id 获取相应的 persona，进而获取 system_prompt

--- main.py
+++ main.py
@@ -142,14 +142,21 @@ class PokeproPlugin(Star):
                 return None
             contexts = json.loads(conversation.history)
 
-            personality = using_provider.curr_personality
-            personality_prompt = personality["prompt"] if personality else ""
+            persona_id = conversation.persona_id
+            # 未使用 /persona 指令切换过人设，获取默认人设id
+            if not persona_id or persona_id == "[%None]":
+                persona = await self.context.persona_manager.get_default_persona_v3(
+                    umo=umo,
+                )
+                persona_id = persona["name"]
+            persona = await self.context.persona_manager.get_persona(persona_id)
+            persona_prompt = persona.system_prompt
             username = self.get_nickname(event, event.get_sender_id())
             format_prompt = prompt_template.format(username=username)
 
             llm_response = await using_provider.text_chat(
                 prompt=format_prompt,
-                system_prompt=personality_prompt,
+                system_prompt=persona_prompt,
                 contexts=contexts,
             )
             return llm_response.completion_text
